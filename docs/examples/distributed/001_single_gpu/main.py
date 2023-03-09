bc6171715e973908f2012bf45b88ef93e376373f
Fabrice Normandin <normandf@mila.quebec>
2023-03-09 14:33:27 -0500

    [temp] Add changes to try git diff
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- docs/examples/distributed/001_single_gpu/main.py
+++ docs/examples/distributed/001_single_gpu/main.py
@@ -1,6 +1,7 @@
 import os
 
 import torch
+from torch import Tensor, nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import CIFAR10
@@ -14,13 +15,13 @@ def main():
     assert torch.cuda.is_available() and torch.cuda.device_count() > 0
     device = torch.device("cuda", 0)
 
+    batch_size = 128
+    training_epochs = 5
+    num_workers = get_num_workers()
+
     # Obtain CIFAR10
     dataset_path = os.environ.get("SLURM_TMPDIR", "../dataset")
-
-    # NOTE: We don't use image transforms here for simplicity.
-    dataset = CIFAR10(root=dataset_path, transform=ToTensor(), train=True)
-    val_split = 0.1
-    training_dataset, validation_dataset = random_split(dataset, ((1 - val_split), val_split))
+    training_dataset, validation_dataset, test_dataset = make_datasets(dataset_path)
 
     # Create a model and move it to the GPU.
     model = resnet18(num_classes=10)
@@ -28,11 +29,15 @@ def main():
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-    batch_size = 128
-    training_dataloader = DataLoader(training_dataset, batch_size=batch_size, shuffle=True)
-    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size)
+    training_dataloader = DataLoader(
+        training_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True
+    )
+    validation_dataloader = DataLoader(
+        validation_dataset, num_workers=num_workers, batch_size=batch_size
+    )
+    # NOTE: Not used in this example
+    test_dataloader = DataLoader(test_dataset, num_workers=num_workers, batch_size=batch_size)
 
-    training_epochs = 1
     batches_per_epoch = len(training_dataloader)
     for epoch in range(training_epochs):
         print(f"Starting epoch {epoch}/{training_epochs}")
@@ -65,29 +70,51 @@ def main():
             progress_bar.set_postfix(loss=loss.item(), accuracy=accuracy.item())
         progress_bar.close()
 
-        # Validation loop
-        model.eval()
-        epoch_val_loss = 0.0
-        correct_predictions = 0
-        total_predictions = 0
-        for batch in validation_dataloader:
-            batch = tuple(item.to(device) for item in batch)
-            x, y = batch
-            with torch.no_grad():
-                logits = model(x)
-                validation_loss = F.cross_entropy(logits, y)
-
-            epoch_val_loss += validation_loss.item()
-            correct_predictions += logits.argmax(-1).eq(y).sum().item()
-            total_predictions += y.shape[0]
-
-        validation_accuracy = correct_predictions / total_predictions
-        print(
-            f"Epoch {epoch}: Validation loss: {epoch_val_loss:.3f} accuracy: {validation_accuracy:.2%}"
-        )
+        val_loss, val_accuracy = validation_loop(model, validation_dataloader, device)
+        print(f"Epoch {epoch}: Validation loss: {val_loss:.3f} accuracy: {val_accuracy:.2%}")
 
     print("Done!")
-    # NOTE: You could save
+
+
+@torch.no_grad()
+def validation_loop(model: nn.Module, dataloader: DataLoader, device: torch.device):
+    model.eval()
+    val_loss = 0.0
+    correct_predictions = 0
+    total_predictions = 0
+    for batch in dataloader:
+        batch = tuple(item.to(device) for item in batch)
+        x, y = batch
+        logits: Tensor = model(x)
+        loss = F.cross_entropy(logits, y)
+
+        val_loss += loss.item()
+        correct_predictions += logits.argmax(-1).eq(y).sum().item()
+        total_predictions += y.shape[0]
+
+    val_accuracy = correct_predictions / total_predictions
+    return val_loss, val_accuracy
+
+
+def make_datasets(dataset_path: str, val_split: float = 0.1, val_split_seed: int = 42):
+    # We don't use image transforms here for simplicity.
+    # Having different transformations for train and validation would complicate things a bit.
+    # Later examples will show how to do the train/val/test split properly when using transforms.
+    train_dataset = CIFAR10(root=dataset_path, transform=ToTensor(), train=True)
+    test_dataset = CIFAR10(root=dataset_path, transform=ToTensor(), train=False)
+    train_dataset, valid_dataset = random_split(
+        train_dataset, ((1 - val_split), val_split), torch.Generator().manual_seed(val_split_seed)
+    )
+    return train_dataset, valid_dataset, test_dataset
+
+
+def get_num_workers() -> int:
+    """Gets the optimal number of DatLoader workers to use in the current job."""
+    if "SLURM_CPUS_PER_TASK" in os.environ:
+        return int(os.environ["SLURM_CPUS_PER_TASK"])
+    if hasattr(os, "sched_getaffinity"):
+        return len(os.sched_getaffinity(0))
+    return torch.multiprocessing.cpu_count()
 
 
 if __name__ == "__main__":
--- docs/examples/distributed/002_multi_gpu/main.py
+++ docs/examples/distributed/002_multi_gpu/main.py
@@ -14,30 +14,19 @@ from torch import Tensor, nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader, random_split
 from torch.utils.data.distributed import DistributedSampler
+from torchvision import transforms
 from torchvision.datasets import CIFAR10
 from torchvision.models import resnet18
-from torchvision.transforms import ToTensor
 from tqdm import tqdm
 
 
 def main():
-    # Check GPU is available
-    assert torch.cuda.is_available() and torch.cuda.device_count() > 0
-    assert torch.distributed.is_available()
-    print("PyTorch Distributed available.")
-    print("  Backends:")
-    print(f"    Gloo: {torch.distributed.is_gloo_available()}")
-    print(f"    NCCL: {torch.distributed.is_nccl_available()}")
-    print(f"    MPI:  {torch.distributed.is_mpi_available()}")
-
-    num_workers = 4
     training_epochs = 10
     learning_rate = 5e-4
+    weight_decay = 1e-4
     batch_size = 512
 
-    world_size = int(os.environ.get("WORLD_SIZE", "1"))  # SLURM_NTASKS_PER_NODE
-    rank = int(os.environ.get("RANK", "0"))  # SLURM_PROCID
-    local_rank = int(os.environ.get("LOCAL_RANK", rank % torch.cuda.device_count()))
+    local_rank, rank, world_size = setup_from_open_clip()
 
     logging.basicConfig(
         level=logging.INFO,
@@ -45,40 +34,28 @@ def main():
         handlers=[rich.logging.RichHandler(markup=True)],
     )
 
+    world_size = torch.distributed.get_world_size()
+    rank = torch.distributed.get_rank()
+    local_rank = rank % torch.cuda.device_count()
+    is_master = rank == 0
+
+    num_workers = get_num_workers()
     logger = logging.getLogger(__name__)
     logger.info(f"World size: {world_size}, global rank: {rank}, local rank: {local_rank}")
 
-    # Initialize distributed
-    torch.distributed.init_process_group(
-        backend="nccl",
-        init_method="env://",
-        world_size=world_size,
-        rank=rank,
-    )
-    is_master = torch.distributed.get_rank() == 0
     device = torch.device("cuda", local_rank)
-    from torchvision import transforms
 
-    # Obtain CIFAR10.
     dataset_path = os.environ.get("SLURM_TMPDIR", "../dataset")
-    #   Only master (rank-0) downloads the dataset if needed!
-    train_transform = transforms.Compose(
-        [
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ]
+
+    # Create a model and move it to the GPU for this process.
+    model = resnet18(num_classes=10)
+    model.to(device=device)
+
+    # Wrap the model with DistributedDataParallel
+    # (See https://pytorch.org/docs/stable/nn.html#torch.nn.parallel.DistributedDataParallel)
+    model = nn.parallel.DistributedDataParallel(
+        model, device_ids=[local_rank], output_device=local_rank
     )
-    if is_master:
-        # Download THEN Barrier
-        train_dataset = CIFAR10(dataset_path, transform=train_transform, download=is_master)
-        valid_dataset = CIFAR10(dataset_path, transform=ToTensor(), download=is_master)
-        torch.distributed.barrier()
-    else:  # Barrier  THEN *NO* Download!
-        torch.distributed.barrier()
-        train_dataset = CIFAR10(dataset_path, transform=train_transform, download=False)
-        valid_dataset = CIFAR10(dataset_path, transform=ToTensor(), download=False)
 
     # Batch size is now interpreted as the "global" batch size, across all GPUS.
     if batch_size % world_size != 0 and rank == world_size - 1:
@@ -87,37 +64,18 @@ def main():
     else:
         local_batch_size = batch_size // world_size
 
-    val_split = 0.1
-    val_split_seed = 42
-    # NOTE: Having different transformations for train and validation complicates things quite a
-    # bit.
-    # Make absolutely sure that all workers get the same splits.
-    train_dataset, _ = random_split(
-        train_dataset, ((1 - val_split), val_split), torch.Generator().manual_seed(val_split_seed)
-    )
-    _, valid_dataset = random_split(
-        valid_dataset, ((1 - val_split), val_split), torch.Generator().manual_seed(val_split_seed)
-    )
+    train_dataset, valid_dataset, test_dataset = make_datasets(dataset_path, is_master=is_master)
     # Restricts data loading to a subset of the dataset exclusive to the current process
     train_sampler = DistributedSampler(dataset=train_dataset, shuffle=True)
     valid_sampler = DistributedSampler(dataset=valid_dataset, shuffle=False)
+    test_sampler = DistributedSampler(dataset=test_dataset, shuffle=False)
 
-    # Create a model and move it to the GPU for this process.
-    model = resnet18(num_classes=10)
-    model.to(device=device)
-    # Wrap the model with DistributedDataParallel
-    # (See https://pytorch.org/docs/stable/nn.html#torch.nn.parallel.DistributedDataParallel)
-    model = nn.parallel.DistributedDataParallel(
-        model, device_ids=[local_rank], output_device=local_rank
-    )
-
-    # NOTE: The train DataLoader is created at the start of each epoch
     train_dataloader = DataLoader(
         train_dataset,
         batch_size=local_batch_size,
-        shuffle=False,
-        sampler=train_sampler,
+        shuffle=False,  # shuffling is done in the sampler, not the dataloader.
         num_workers=num_workers,
+        sampler=train_sampler,
     )
     validation_dataloader = DataLoader(
         valid_dataset,
@@ -126,11 +84,18 @@ def main():
         num_workers=num_workers,
         sampler=valid_sampler,
     )
+    test_dataloader = DataLoader(  # note: unused in this example here.
+        test_dataset,
+        batch_size=local_batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        sampler=test_sampler,
+    )
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
     for epoch in range(training_epochs):
-        # NOTE: Here we need to call `set_epoch` so the ordering changes at each epoch.
+        # NOTE: Here we need to call `set_epoch` so the ordering is able to change at each epoch.
         train_sampler.set_epoch(epoch)
 
         progress_bar = tqdm(
@@ -154,21 +119,25 @@ def main():
             # NOTE: nn.DistributedDataParallel automatically averages the gradients across devices.
             optimizer.step()
 
-            local_correct_predictions = logits.detach().argmax(-1).eq(y).sum()
+            # "local" metrics: calculated with the results for this worker
+            local_n_correct_predictions = logits.detach().argmax(-1).eq(y).sum()
             # The actual local batch size (might differ from local_batch_size for the last batch)
-            local_n_samples = torch.as_tensor(logits.shape[0], device=device)
+            local_n_samples = logits.shape[0]
+            local_accuracy = local_n_correct_predictions / local_n_samples
 
+            # "global" metrics: calculated with the results from all workers
             # NOTE: Creating new tensors to hold the "global" values, but this isn't required.
-            n_samples = local_n_samples.clone()  # Will store the effective batch size.
-            loss = local_loss.clone()  # Will store the average loss across all workers.
-            correct_predictions = local_correct_predictions.clone()
-            local_accuracy = local_correct_predictions / local_n_samples
+            n_correct_predictions = local_n_correct_predictions.clone()
+            # Actual (global) batch size for this step.
+            n_samples = torch.as_tensor(local_n_samples, device=device)
+            # Will store the average loss across all workers.
+            loss = local_loss.clone()
 
-            torch.distributed.all_reduce(correct_predictions, op=torch.distributed.ReduceOp.SUM)
+            torch.distributed.all_reduce(n_correct_predictions, op=torch.distributed.ReduceOp.SUM)
             torch.distributed.all_reduce(n_samples, op=torch.distributed.ReduceOp.SUM)
             torch.distributed.all_reduce(loss, op=torch.distributed.ReduceOp.SUM)
-            loss.div_(world_size)  # Divide the loss by the number of workers.
-            accuracy = correct_predictions / n_samples
+            loss.div_(world_size)  # Report the average loss across all workers.
+            accuracy = n_correct_predictions / n_samples
 
             logger.debug(f"(local) accuracy: {local_accuracy:.2%}")
             logger.debug(f"(local) loss: {local_loss.item()}")
@@ -189,42 +158,156 @@ def main():
             )
 
     print("Done!")
-    # NOTE: You could save
 
 
 @torch.no_grad()
 def validation_loop(
     model: nn.Module,
-    validation_dataloader: DataLoader,
+    dataloader: DataLoader,
     device: torch.device,
 ):
     model.eval()
 
-    epoch_val_loss = torch.as_tensor(0.0, device=device)
-    epoch_n_samples = torch.as_tensor(0, device=device)
-    epoch_correct_predictions = torch.as_tensor(0, device=device)
+    val_loss = torch.as_tensor(0.0, device=device)
+    n_samples = torch.as_tensor(0, device=device)
+    correct_predictions = torch.as_tensor(0, device=device)
 
-    for local_batch in validation_dataloader:
+    for local_batch in dataloader:
         local_batch = tuple(item.to(device) for item in local_batch)
         x, y = local_batch
 
         logits: Tensor = model(x)
 
         loss = F.cross_entropy(logits, y)
-        n_samples = x.shape[0]
-        correct_predictions = logits.argmax(-1).eq(y).sum()
+        batch_n_samples = x.shape[0]
+        batch_correct_predictions = logits.argmax(-1).eq(y).sum()
 
-        epoch_val_loss += loss
-        epoch_n_samples += n_samples
-        epoch_correct_predictions += correct_predictions
+        val_loss += loss
+        n_samples += batch_n_samples
+        correct_predictions += batch_correct_predictions
 
-    # Sum up the metrics we gathered on each worker before returning them.
-    torch.distributed.all_reduce(epoch_val_loss, op=torch.distributed.ReduceOp.SUM)
-    torch.distributed.all_reduce(epoch_correct_predictions, op=torch.distributed.ReduceOp.SUM)
-    torch.distributed.all_reduce(epoch_n_samples, op=torch.distributed.ReduceOp.SUM)
+    # Sum up the metrics we gathered on each worker before returning the overall val metrics.
+    torch.distributed.all_reduce(val_loss, op=torch.distributed.ReduceOp.SUM)
+    torch.distributed.all_reduce(correct_predictions, op=torch.distributed.ReduceOp.SUM)
+    torch.distributed.all_reduce(n_samples, op=torch.distributed.ReduceOp.SUM)
 
-    validation_accuracy = epoch_correct_predictions / epoch_n_samples
-    return epoch_val_loss, validation_accuracy
+    validation_accuracy = correct_predictions / n_samples
+    return val_loss, validation_accuracy
+
+
+def setup():
+    if "WORLD_SIZE" in os.environ:
+        # Job launched with `torchrun`:
+        world_size = int(os.environ["WORLD_SIZE"])
+        rank = int(os.environ["RANK"])
+    else:
+        # Job launched with `srun`
+        world_size = int(os.environ["SLURM_NTASKS"])
+        rank = int(os.environ["SLURM_PROCID"])
+
+    # Initialize distributed
+    torch.distributed.init_process_group(
+        backend="nccl",
+        init_method="env://",
+        world_size=world_size,
+        rank=rank,
+    )
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=f"[{rank}/{world_size}] %(name)s - %(message)s ",
+        handlers=[rich.logging.RichHandler(markup=True)],
+    )
+
+
+def setup_from_open_clip():
+    # Check GPU is available
+    assert torch.cuda.is_available() and torch.cuda.device_count() > 0
+    assert torch.distributed.is_available()
+    print("PyTorch Distributed available.")
+    print("  Backends:")
+    print(f"    Gloo: {torch.distributed.is_gloo_available()}")
+    print(f"    NCCL: {torch.distributed.is_nccl_available()}")
+    print(f"    MPI:  {torch.distributed.is_mpi_available()}")
+
+    if "SLURM_PROCID" in os.environ:
+        # DDP Job is being run via `srun` on a slurm cluster.
+        local_rank = int(os.environ["SLURM_LOCALID"])
+        rank = int(os.environ["SLURM_PROCID"])
+        world_size = int(os.environ["SLURM_NTASKS"])
+
+        # SLURM var -> torch.distributed vars in case needed
+        # NOTE: Setting these values isn't exactly necessary, but some code might assume it's
+        # being run via torchrun or torch.distributed.launch, so setting these can be a good idea.
+        os.environ["LOCAL_RANK"] = str(local_rank)
+        os.environ["RANK"] = str(rank)
+        os.environ["WORLD_SIZE"] = str(world_size)
+
+        torch.distributed.init_process_group(
+            backend="nccl",
+            init_method="env://",
+            world_size=world_size,
+            rank=rank,
+        )
+    else:
+        # DDP via torchrun, torch.distributed.launch
+        local_rank = int(os.environ.get("LOCAL_RANK", 0))
+        torch.distributed.init_process_group(backend="nccl", init_method="env://")
+        world_size = torch.distributed.get_world_size()
+        rank = torch.distributed.get_rank()
+    return local_rank, rank, world_size
+
+
+def make_datasets(
+    dataset_path: str,
+    is_master: bool,  # Added: Only master (rank-0) downloads the dataset if needed!
+    val_split=0.1,
+    val_split_seed=42,
+):
+    # Obtain CIFAR10.
+    normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]
+    )
+    val_transform = transforms.Compose([transforms.ToTensor(), normalize])
+    # Only master (rank-0) downloads the dataset if necessary.
+    if is_master:
+        # Download (if necessary) THEN Barrier
+        train_dataset = CIFAR10(dataset_path, transform=train_transform, download=True)
+        valid_dataset = CIFAR10(dataset_path, transform=val_transform, download=True)
+        test_dataset = CIFAR10(dataset_path, transform=val_transform, download=True, train=False)
+        torch.distributed.barrier()
+    else:
+        # Barrier  THEN *NO* Download!
+        torch.distributed.barrier()
+        train_dataset = CIFAR10(dataset_path, transform=train_transform, download=False)
+        valid_dataset = CIFAR10(dataset_path, transform=val_transform, download=False)
+        test_dataset = CIFAR10(dataset_path, transform=val_transform, train=False, download=False)
+
+    # NOTE: Having different transformations for train and validation complicates things quite a
+    # bit.
+    # Make absolutely sure that all workers get the same splits.
+    train_dataset, _ = random_split(
+        train_dataset, ((1 - val_split), val_split), torch.Generator().manual_seed(val_split_seed)
+    )
+    _, valid_dataset = random_split(
+        valid_dataset, ((1 - val_split), val_split), torch.Generator().manual_seed(val_split_seed)
+    )
+    return train_dataset, valid_dataset, test_dataset
+
+
+def get_num_workers() -> int:
+    """Gets the optimal number of DatLoader workers to use in the current job."""
+    if "SLURM_CPUS_PER_TASK" in os.environ:
+        return int(os.environ["SLURM_CPUS_PER_TASK"])
+    if hasattr(os, "sched_getaffinity"):
+        return len(os.sched_getaffinity(0))
+    return torch.multiprocessing.cpu_count()
 
 
 if __name__ == "__main__":
