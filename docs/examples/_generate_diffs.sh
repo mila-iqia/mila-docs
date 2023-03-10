59d50fa16a9899e928e06da52157d5c488358c48
Fabrice Normandin <normandf@mila.quebec>
2023-03-09 19:03:00 -0500

    Add todo for fixing whitespace issues in diffs
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- docs/examples/_generate_diffs.sh
+++ docs/examples/_generate_diffs.sh
@@ -4,20 +4,32 @@
 # that depend on it.
 
 
-git diff --no-index \
+# TODO: Unsure if it woulb be easier to use `diff` or `git diff` here.
+# `git diff` adds some 'index' lines that change between commits for no real reason.
+
+# diff docs/examples/distributed/001_single_gpu/job.sh \
+#      docs/examples/distributed/002_multi_gpu/job.sh \
+#      > docs/examples/distributed/002_multi_gpu/job.sh.diff
+# diff docs/examples/distributed/001_single_gpu/main.py \
+#      docs/examples/distributed/002_multi_gpu/main.py \
+#      > docs/examples/distributed/002_multi_gpu/main.py.diff
+
+# TODO: Running this adds whitespace in the diffs even if the files are identical.
+
+git diff --no-index --ignore-space-change \
     docs/examples/distributed/001_single_gpu/job.sh \
     docs/examples/distributed/002_multi_gpu/job.sh \
     > docs/examples/distributed/002_multi_gpu/job.sh.patch
-git diff --no-index \
+git diff --no-index --ignore-space-change \
     docs/examples/distributed/001_single_gpu/main.py \
     docs/examples/distributed/002_multi_gpu/main.py \
     > docs/examples/distributed/002_multi_gpu/main.py.patch
 
-git diff --no-index \
+git diff --no-index --ignore-space-change \
     docs/examples/distributed/002_multi_gpu/job.sh \
     docs/examples/distributed/003_multi_node/job.sh \
     > docs/examples/distributed/003_multi_node/job.sh.patch
-git diff --no-index \
+git diff --no-index --ignore-space-change \
     docs/examples/distributed/002_multi_gpu/main.py \
     docs/examples/distributed/003_multi_node/main.py \
     > docs/examples/distributed/003_multi_node/main.py.patch
