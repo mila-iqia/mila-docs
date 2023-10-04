c67719325a06f0c5c1dd79a514057bf83e586a81
Setepenre <pierre.delaunay.tr@gmail.com>
2023-10-04 15:11:36 +0000

    Remove out-dated comment

--- docs/examples/llm/inference_server.sh
+++ docs/examples/llm/inference_server.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#
-# Assume you have conda installed
-#
-# Usage:
-#   
-#     sbatch --ntasks-per-node=1 --mem=32G inference_server_SHARED.sh meta/Llama-2-7b-chat-hf
-#     sbatch --ntasks-per-node=2 --mem=64G inference_server_SHARED.sh meta/Llama-2-13b-chat-hf
-#     sbatch --ntasks-per-node=8 --mem=192G inference_server_SHARED.sh meta/Llama-2-70b-chat-hf
-#
-
 #SBATCH --gpus-per-task=rtx8000:1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=00:15:00
@@ -19,8 +9,8 @@
 function usage() {
   echo "Usage: $0 [-m] [-p]"
   echo "  -h              Display this help message."
-  echo "  -m MODEL        Specify a file to process."
-  echo "  -p PATH         Specify a directory to work in."
+  echo "  -m MODEL        Specify the model name"
+  echo "  -p PATH         Specify the model weights"
   echo "  -e ENV          Specify the conda environementt to use."
   echo "  ARGUMENT        Any additional argument you want to process."
   exit 1
@@ -75,7 +65,7 @@ source $CONDA_BASE/../etc/profile.d/conda.sh
 #   Create a new environment
 #
 if [ ! -d "$ENV" ] && [ "$ENV" != "base" ] && [ ! -d "$CONDA_ENVS/$ENV" ]; then
-     conda create --prefix $ENV python=3.9 -y 
+     conda create --prefix $ENV python=3.9 -y
 fi
 conda activate $ENV
 pip install vllm
