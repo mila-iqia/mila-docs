84524dddb70d0297d3bb4a06e4526de2e2b960fd
notoraptor <notoraptor@users.noreply.github.com>
2023-11-06 13:48:41 -0500

    Doc 80 - exemple lancer plusieurs jobs (#222)
    * Add an example to launch many jobs using same shelle script.
    
    * Fix doc ref.
    
    * Add context.
    
    * Update docs/examples/good_practices/launch_many_jobs/README.rst
    
    Co-authored-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * Update docs/examples/good_practices/launch_many_jobs/main.py
    
    Co-authored-by: Fabrice Normandin <fabrice.normandin@gmail.com>
    
    * Fix doc.
    
    * Prevent argument splitting when passing arguments to main.py
    
    * Rename section to put it before Orion example.
    
    * Rename back example folder to `launch_many_jobs`
    
    * Manually order good_practices examples in documentation
    
    ---------
    
    Co-authored-by: Fabrice Normandin <fabrice.normandin@gmail.com>

--- docs/examples/generate_diffs.sh
+++ docs/examples/generate_diffs.sh
@@ -50,4 +50,8 @@ generate_diff distributed/single_gpu/main.py good_practices/wandb_setup/main.py
 generate_diff distributed/single_gpu/job.sh frameworks/jax/job.sh
 generate_diff distributed/single_gpu/main.py frameworks/jax/main.py
 
+# single_gpu -> launch_many_jobs
+generate_diff distributed/single_gpu/job.sh good_practices/launch_many_jobs/job.sh
+generate_diff distributed/single_gpu/main.py good_practices/launch_many_jobs/main.py
+
 popd
