Bugfix GPU ordinal computation
With `--gpus-per-task=rtx8000:1`, all tasks only see `1` GPU. Therefore, the only valid GPU ordinal is `0`, even if each task sees a different single GPU.

For extra robustness, use the common trick of calculating the ordinal as `ordinal = rank % device_count()`, which works both in and outside of SLURM.

docs/examples/distributed/multi_gpu/main.py
@@ -37,7 +37,7 @@ def main():
     assert torch.cuda.is_available() and torch.cuda.device_count() > 0
     rank, world_size = setup()
     is_master = rank == 0
-    device = torch.device("cuda", rank)
+    device = torch.device("cuda", rank % torch.cuda.device_count())
 
     # Setup logging (optional, but much better than using print statements)
     logging.basicConfig(

docs/examples/distributed/multi_node/main.py
@@ -39,7 +39,7 @@ def main():
     rank, world_size, local_rank = setup()
     is_master = rank == 0
     is_local_master = local_rank == 0
-    device = torch.device("cuda", local_rank)
+    device = torch.device("cuda", local_rank % torch.cuda.device_count())
 
     # Setup logging (optional, but much better than using print statements)
     logging.basicConfig(